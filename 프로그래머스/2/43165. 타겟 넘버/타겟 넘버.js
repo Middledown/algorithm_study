9353b3dde35771bbb8eed73845a67b7f1db9a81e
middledown <124867768+Middledown@users.noreply.github.com>
2023-12-06 14:47:03 +0900

    [level 2] Title: 타겟 넘버, Time: 1.07 ms, Memory: 35.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.js
@@ -0,0 +1,18 @@
+function solution(numbers, target) {
+  let answer = 0;
+
+  function dfs(count, sum) {
+    if (count === numbers.length) {
+      if (target === sum) {
+        answer++;
+      }
+      return;
+    }
+    dfs(count + 1, sum + numbers[count]);
+    dfs(count + 1, sum - numbers[count]);
+  }
+
+  dfs(0, 0);
+
+  return answer;
+}
