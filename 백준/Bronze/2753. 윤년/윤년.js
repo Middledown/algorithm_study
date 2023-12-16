eef32bbccec174a8859854a24fedd1ba3de528a6
middledown <124867768+Middledown@users.noreply.github.com>
2023-12-16 23:58:25 +0900

    [Bronze V] Title: 윤년, Time: 136 ms, Memory: 9344 KB -BaekjoonHub

---
+++ 백준/Bronze/2753. 윤년/윤년.js
@@ -0,0 +1,8 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+let input = fs.readFileSync(filePath).toString().split("\n");
+
+input = +input[0];
+if ((input % 4 === 0 && input % 100 !== 0) || input % 400 === 0) {
+  console.log(1);
+} else console.log(0);
