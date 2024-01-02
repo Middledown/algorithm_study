04dce84335578c2f6492e56da7d091576ab2ad3a
middledown <124867768+Middledown@users.noreply.github.com>
2024-01-02 14:22:30 +0900

    [Bronze III] Title: 최소, 최대, Time: 1052 ms, Memory: 104760 KB -BaekjoonHub

---
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.js
@@ -0,0 +1,9 @@
+const fs = require("fs");
+const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+
+const arr = input[1].split(" ").map((value) => +value);
+arr.sort(function sort1(a, b) {
+  return a - b;
+});
+
+console.log(arr[0], arr.pop());
