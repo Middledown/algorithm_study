bcbe91805309481a0d3feed3deefe7bf79ad50e5
middledown <124867768+Middledown@users.noreply.github.com>
2024-01-08 16:39:48 +0900

    [Bronze II] Title: 나머지, Time: 120 ms, Memory: 9324 KB -BaekjoonHub

---
+++ 백준/Bronze/3052. 나머지/나머지.js
@@ -0,0 +1,14 @@
+const fs = require("fs");
+const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+
+const userNum = [];
+
+input.forEach((x) => {
+  const num = x % 42;
+
+  if (userNum.indexOf(num) === -1) {
+    userNum.push(num);
+  }
+});
+
+console.log(userNum.length);
