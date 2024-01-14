5898bf574c01a1a2a0c8f72e5bb99609034ae950
middledown <124867768+Middledown@users.noreply.github.com>
2024-01-14 17:06:17 +0900

    [Bronze V] Title: 아스키 코드, Time: 124 ms, Memory: 9336 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.js
@@ -0,0 +1,8 @@
+const fs = require("fs");
+const file = process.platform === "linux" ? "dev/stdin" : "input.txt";
+const input = fs.readFileSync(file).toString().split(" ");
+
+let str = input.toString();
+result = str.charCodeAt(0);
+
+console.log(result);
