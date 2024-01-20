3da428f9339940ec27628e9decb28597e13132c2
middledown <124867768+Middledown@users.noreply.github.com>
2024-01-20 23:55:10 +0900

    [Bronze II] Title: 단어의 개수, Time: 204 ms, Memory: 18016 KB -BaekjoonHub

---
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.js
@@ -0,0 +1,13 @@
+const fs = require("fs");
+const file = process.platform === "linux" ? "dev/stdin" : "input.txt";
+const input = fs.readFileSync(file).toString().trim().split(" ");
+
+let answer = 0;
+
+for (let i = 0; i < input.length; i++) {
+  if (input[i] !== "") {
+    answer++;
+  }
+}
+
+console.log(answer);
