13a269ccc3c1bd63db29a113f82de0da2b01eb4a
middledown <124867768+Middledown@users.noreply.github.com>
2024-01-31 17:27:10 +0900

    [level 0] Title: 문자열 뒤집기, Time: 13.07 ms, Memory: 77.2 MB -BaekjoonHub

--- 프로그래머스/0/120822. 문자열 뒤집기/문자열 뒤집기.java
+++ 프로그래머스/0/120822. 문자열 뒤집기/문자열 뒤집기.java
@@ -2,8 +2,8 @@ class Solution {
     public String solution(String my_string) {
         String answer = "";
         int len = my_string.length();
-        for(int i = 0; i < len; i++) {
-            answer += my_string.substring(len - i - 1, len - i);
+        for(int i = len - 1; i >= 0; i--) {
+            answer += my_string.charAt(i);
         }
         return answer;
     }
