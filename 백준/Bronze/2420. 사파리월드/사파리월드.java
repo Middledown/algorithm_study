a7556b20568e12505707211ebd321eaa17f4a8bd
middledown <124867768+Middledown@users.noreply.github.com>
2024-02-06 18:52:09 +0900

    [Bronze V] Title: 사파리월드, Time: 120 ms, Memory: 14256 KB -BaekjoonHub

---
+++ 백준/Bronze/2420. 사파리월드/사파리월드.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.math.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] input = br.readLine().split(" ");
+        long N = Long.parseLong(input[0]);
+        long M = Long.parseLong(input[1]);
+        
+        System.out.println(Math.abs(M - N));
+    }
+}
