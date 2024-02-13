3c9ce27ffbaa6858f50a6a35362f841440ea98c9
middledown <124867768+Middledown@users.noreply.github.com>
2024-02-13 12:01:30 +0900

    [level 0] Title: 외계어 사전, Time: 0.06 ms, Memory: 71.6 MB -BaekjoonHub

--- 프로그래머스/0/120869. 외계어 사전/외계어 사전.java
+++ 프로그래머스/0/120869. 외계어 사전/외계어 사전.java
@@ -1,20 +1,13 @@
 class Solution {
     public int solution(String[] spell, String[] dic) {
-        int answer = 2;
+        
         for(int i = 0; i < dic.length; i++) {
+            int answer = 0;
             for(int j = 0; j < spell.length; j++) {
-				if (dic[i].contains(spell[j])) {
-					dic[i] = dic[i].replaceFirst(spell[j], "");
-				} else {
-					dic[i] += spell[j];
-				}
+				if (dic[i].contains(spell[j])) answer++;
             }
+            if(answer == spell.length) return 1;
         }
-        for(int i = 0; i < dic.length; i++) {
-            if(dic[i].equals("")) {
-                answer = 1;
-            }
-        }
-        return answer;
+        return 2;
     }
 }
