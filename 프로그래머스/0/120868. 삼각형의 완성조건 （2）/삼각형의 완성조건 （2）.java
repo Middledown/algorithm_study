2d95a6322df501bcd7db65d85a8b74181beac5ca
middledown <124867768+Middledown@users.noreply.github.com>
2024-02-13 10:30:01 +0900

    [level 0] Title: 삼각형의 완성조건 (2), Time: 0.02 ms, Memory: 73.9 MB -BaekjoonHub

--- 프로그래머스/0/120868. 삼각형의 완성조건 （2）/삼각형의 완성조건 （2）.java
+++ 프로그래머스/0/120868. 삼각형의 완성조건 （2）/삼각형의 완성조건 （2）.java
@@ -1,7 +1,6 @@
 class Solution {
     public int solution(int[] sides) {
         int answer = 0;
-        // 가장 긴 변이 sides 중 하나일 경우
         int max = sides[0] > sides[1] ? sides[0] : sides[1];
         int min = sides[0] > sides[1] ? sides[1] : sides[0];
         for(int i = 1; i < max + min; i++) {
@@ -9,9 +8,6 @@ class Solution {
                 answer++;
             }   
         }
-        
-        
-        // 나머지 한 변이 가장 긴 변인 경우
         
         return answer;
     }
