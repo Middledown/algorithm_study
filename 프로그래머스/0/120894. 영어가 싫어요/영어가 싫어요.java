9fadbc8d7561d6fd2b30a2bde9220fa2f65a5893
middledown <124867768+Middledown@users.noreply.github.com>
2024-02-13 09:39:25 +0900

    [level 0] Title: 영어가 싫어요, Time: 7.15 ms, Memory: 75.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120894. 영어가 싫어요/영어가 싫어요.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+
+class Solution {
+    public long solution(String numbers) {
+        String [] arr = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+        for(int i = 0; i < arr.length; i++) {
+            if(numbers.contains(arr[i])) {
+                numbers = numbers.replaceAll(arr[i], i + "");
+            }
+        }
+        long answer = Long.parseLong(numbers);
+        return answer;
+    }
+}
