17f2b58e33a7b7093d6dc70d98a7c0614372aa40
middledown <124867768+Middledown@users.noreply.github.com>
2024-02-13 10:16:31 +0900

    [level 0] Title: 구슬을 나누는 경우의 수, Time: 0.94 ms, Memory: 76.1 MB -BaekjoonHub

--- 프로그래머스/0/120840. 구슬을 나누는 경우의 수/구슬을 나누는 경우의 수.java
+++ 프로그래머스/0/120840. 구슬을 나누는 경우의 수/구슬을 나누는 경우의 수.java
@@ -3,19 +3,16 @@ import java.math.*;
 class Solution {
     public int solution(int n, int m) {
         BigInteger facN = new BigInteger("1");
-        BigInteger facM = new BigInteger("1");
         BigInteger facNM = new BigInteger("1");
         
-        for(int i = n; i > 1; i--) {
+        for(int i = n; i > m; i--) {
             facN = facN.multiply(BigInteger.valueOf(i));
-        }
-        for(int i = m; i > 1; i--) {
-            facM = facM.multiply(BigInteger.valueOf(i));
         }
         for(int i = n - m; i > 1; i--) {
             facNM = facNM.multiply(BigInteger.valueOf(i));
         }
-        int answer = (facN.divide(facNM.multiply(facM))).intValue();
+        
+        int answer = (facN.divide(facNM)).intValue();
         return answer;
     }
 }
