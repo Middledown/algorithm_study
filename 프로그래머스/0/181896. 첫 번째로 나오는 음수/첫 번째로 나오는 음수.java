c490a4fe05a7e10d6f2a6139644f7726eaf89459
middledown <124867768+Middledown@users.noreply.github.com>
2024-02-20 13:50:51 +0900

    [level 0] Title: 첫 번째로 나오는 음수, Time: 0.01 ms, Memory: 71.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(int[] num_list) {
+        int answer = -1;
+        for(int i = 0; i < num_list.length; i++) {
+            if(0 > num_list[i]) {
+                answer = i;
+                break;
+            }
+        }
+        return answer;
+    }
+}
