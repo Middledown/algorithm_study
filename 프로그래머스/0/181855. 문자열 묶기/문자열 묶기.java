1f8a144267175567f0ad676936f0b78ddb875d0b
middledown <124867768+Middledown@users.noreply.github.com>
2024-03-08 10:36:21 +0900

    [level 0] Title: 문자열 묶기, Time: 9.06 ms, Memory: 123 MB -BaekjoonHub

---
+++ 프로그래머스/0/181855. 문자열 묶기/문자열 묶기.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String[] strArr) {
+        int[] arr = new int[100000];
+        for(int i = 0; i < strArr.length; i++) {
+            arr[strArr[i].length()]++;    
+        }
+        
+        return Arrays.stream(arr).max().getAsInt();
+    }
+}
